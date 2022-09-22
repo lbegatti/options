61cd7f9ce5e5417d22801ae370df865149a0f924
Luca Begatti <luca.begatti93@gmail.com>
2022-09-22 17:58:49 +0200

    Create option.py
    Option input values and payoff call.

---
+++ python/Vanilla_Asian_calloptionPricing/option.py
@@ -0,0 +1,11 @@
+from pricingExercise import Payoff
+
+
+class Option:
+    def __init__(self, payoff_type: Payoff, expiry, option_type):
+        self.expiry = expiry
+        self.payoff_type = payoff_type
+        self.option_type = option_type
+
+    def payoff(self, spot):
+        return self.payoff_type.payoff(spot)
