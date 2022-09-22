a265b6141b06cfc364e3d1f85d457a7248f885c2
Luca Begatti <luca.begatti93@gmail.com>
2022-09-22 18:04:46 +0200

    Create main.py
    main.py delivers the price for the vanilla call option and the Asian (arithmetic option). There is also the Black-Scholes analytical solution just to be compared with the vanilla option price via Monte Carlo simulation.  The analytical solution, should not, therefore, be compared to the Asian option value.

---
+++ python/Vanilla_Asian_calloptionPricing/main.py
@@ -0,0 +1,31 @@
+from pricingExercise.Payoff import Payoff
+from pricingExercise.option import Option
+from pricingExercise.gbm import GBM
+from pricingExercise.blackscholes import BS
+
+# We make some assumptions about the option
+strike = 105
+spot = 110
+expiry = 1
+sigma = 0.15
+r = 0.02
+paths = 1500
+timesteps = 126
+
+# Instantiate class constructors and price the option
+call = Payoff(strike=strike)
+gbm = GBM(spot=spot, r=r, sigma=sigma, paths=paths, timesteps=timesteps)
+european_call = gbm.option_value(Option(payoff_type=call, expiry=expiry, option_type='european'))
+analytical_price = BS(spot, strike, expiry, r, sigma).call_price()
+asian_call = gbm.option_value(Option(payoff_type=call, expiry=expiry, option_type='asian'))
+
+print("The Monte Carlo price is: {}".format(round(european_call, 2)))
+print("The analytical price is: {}".format(round(analytical_price, 2)))
+print("The Monte Carlo Asian price is: {}".format(round(asian_call, 2)))
+
+""" Notes 
+Asian pricer < European price. This makes sense based on the model assumptions we have, 
+there is positive interest rate (drift) and high volatility so S_T will with high probability be higher than all 
+the previous values so therefore the AVERAGE(S_t) is most likely lower than S_T and thereby will the price
+you pay also be lower.
+"""
